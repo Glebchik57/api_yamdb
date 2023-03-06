d5f1471806bd1ff2910c6a2df3e5628ca3a931fd
Севостьянов Глеб <g.sevostyanov@inbox.ru>
2023-03-06 22:47:17 +0200

    serializeer was added

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
-# Create your views here.
+from reviews.models import Review
+from .serializers import CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
