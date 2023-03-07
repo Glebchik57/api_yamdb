3f258e56728a55e5ad038ac5c9572067340dd401
Anastasiya Kurnosova <AK.johnny@yandex.ru>
2023-03-08 00:18:24 +0300

    Added UserView.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,8 +4,18 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
     class Meta:
         fields = (
             'username', 'email', 'first_name', 'last_name', 'bio', 'role',
         )
         model = User
+
+
+class NewUserRegistrationSerializer(UserSerializer):
+
+    class Meta(UserSerializer.Meta):
+        read_only_fields = ['role']
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -24,6 +24,7 @@ class User(AbstractUser):
     )
     email = models.EmailField(
         unique=True,
+        max_length=254,
     )
 
     class Meta(AbstractUser.Meta):
