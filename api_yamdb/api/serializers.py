e60f7801123f0d5b72bf52fcb6787f7260ca2b0f
Anastasiya Kurnosova <AK.johnny@yandex.ru>
2023-03-08 15:12:25 +0300

    Change serializers.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,8 +4,48 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
     class Meta:
         fields = (
             'username', 'email', 'first_name', 'last_name', 'bio', 'role',
+        )
+        model = User
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'You can`t use "me"'
+            )
+        return value
+
+    def validate_role(self, value):
+        if value in ['admin', 'user', 'moderator']:
+            return value
+        raise serializers.ValidationError(
+            'There is no such role'
+        )
+
+
+class UserRegistrationSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField(max_length=254)
+
+    class Meta:
+        fields = (
+            'username', 'email',
+        )
+        # model = User
+
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    confirmation_code = serializers.CharField()
+
+    class Meta:
+        fields = (
+            'username', 'confirmation_code',
         )
         model = User
