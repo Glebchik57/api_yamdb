66ddc1f52469cf54f24f3446bd956efa804ee74a
Anastasiya Kurnosova <AK.johnny@yandex.ru>
2023-03-08 15:50:37 +0300

    Change views.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-# from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from api.views import UserViewSet, get_token, sign_up
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,19 +1,66 @@
-# from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 # from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from rest_framework import viewsets, status, filters, permissions
+from rest_framework.pagination import LimitOffsetPagination
 from reviews.models import User
-from rest_framework import viewsets
-# from .permission import User
-from .serializers import UserSerializer
+from .permission import UserIsAdmin
+from .serializers import (UserSerializer,
+                          UserRegistrationSerializer,
+                          NewUserRegistrationSerializer,
+                          TokenSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [UserIsAdmin]
+
+    lookup_field = 'username'
+    pagination_class = LimitOffsetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'role']
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    @action(
+        methods=['GET', 'PATCH'],
+        detail=False,
+        permission_classes=[permissions.IsAuthenticated],
+        url_name='me',
+    )
+    def me(self, request, *args, **kwargs):
+        if request.method == 'PATCH':
+            serializer = NewUserRegistrationSerializer(
+                request.user, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        serializer = NewUserRegistrationSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+# @api_view(['POST'])
 def sign_up(request):
     pass
+#     serializer = UserRegistrationSerializer(data=request.data)
+#     serializer.is_valid(raise_exception=True)
+    # validated_data = serializer.data
+    # user = User.objects.filter(username=validated_data['username'])
 
 
+@api_view(['POST'])
 def get_token(request):
-    pass
+    serializer = TokenSerializer(data=request.data)
+    serializer.is_valid(
+        raise_exception=True
+    )
+    user = get_object_or_404(User, username=serializer.data['username'])
+    if serializer.data['confirmation_code'] == user.confirmation_code:
+        token = RefreshToken.for_user(user)
+        return Response({'token': str(token.access_token)},
+                        status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
