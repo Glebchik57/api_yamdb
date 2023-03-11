4a7a4e7a1d710c392c4f62b5588ba12074ebf2e5
SoykaShir <MayerFox@mail.ru>
2023-03-11 14:29:10 +0300

    create load_csv

---
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -0,0 +1,32 @@
+from csv import DictReader
+from django.core.management import BaseCommand
+from reviews.models import (Categories, Genres,
+                            Titles, Review, Comment,
+                            User)
+import os
+from django.conf import settings
+
+MODEL_LST = {
+    'category.csv': Categories,
+    'genre.csv': Genres,
+    'titles.csv': Titles,
+    'comments.csv': Comment,
+    'review.csv': Review,
+    'users.csv': User
+}
+
+
+class Command(BaseCommand):
+    help = 'Читает csv файл и заносит в базу данных'
+
+    def handle(self, *args, **options):
+        for csvname, mod in MODEL_LST.items():
+            path = os.path.join(settings.BASE_DIR, f"static/data/{csvname}")
+            with open(path, encoding='utf-8') as file:
+                reader = DictReader(file)
+                mod.objects.all().delete()
+                header = [i for i in reader]
+                for row in reader:
+                    _object_dict = {key: value for key, value in zip(header,
+                                                                     row)}
+                    mod.objects.bulk_create([mod(**_object_dict)])
