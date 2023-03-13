e940f9f1ade8e041705f30493ca86ed7f1d6f3cf
Севостьянов Глеб <g.sevostyanov@inbox.ru>
2023-03-13 23:48:31 +0200

    remarks was corrected

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,13 +10,15 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and (
+        return (
+            request.method in permissions.SAFE_METHODS or (
+                request.user.is_authenticated and (
                     obj.author == request.user
                     or request.user.role == 'moderator'
-                    or request.user.role == 'admin')
-                 )
+                    or request.user.role == 'admin'
                 )
+            )
+        )
 
 
 class UserIsAdmin(IsAuthenticatedOrReadOnly):
